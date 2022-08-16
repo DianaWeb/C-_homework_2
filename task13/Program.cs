dd5fdc42b1b373833814c74bf3542db16fb2ce68
«Diana» <diana-mk@ya.ru>
2022-08-16 16:20:11 +0700

    Cделали задачу 13

--- task13/Program.cs
+++ task13/Program.cs
@@ -6,4 +6,20 @@
 
 32679 -> 6 */
 
-// Не знаю как решать. 
+System.Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+number = Math.Abs(number);
+
+if (number >= 100)
+{
+	while (number >= 1000)
+	{
+		number = number / 10;
+	}
+	int digit3 = number % 10;
+	System.Console.WriteLine(digit3);
+}
+else
+{
+	System.Console.WriteLine("В числе нет третьей цифры.");
+}
